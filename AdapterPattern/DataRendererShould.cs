a2d16c9a0f3f88b851862df1c88778708a6f073d
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-13 15:24:37 +0800

    Added Adapter Pattern project.

---
+++ AdapterPattern/DataRendererShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdapterPattern.Library;
+using AdapterPattern.Test;
+using System.IO;
+using System.Linq;
+
+namespace AdapterPattern
+{
+    [TestClass]
+    public class DataRendererShould
+    {
+        [TestMethod]
+        public void RenderOneRowGivenStubDataAdapter()
+        {
+            var myRenderer = new DataRenderer(new StubDbAdapter());
+
+            var writer = new StringWriter();
+            myRenderer.Render(writer);
+
+            string result = writer.ToString();
+            Console.Write(result);
+
+            int lineCount = result.Count(c => c == '\n');
+            Assert.AreEqual(3, lineCount);
+        }
+    }
+}
---
+++ AdapterPattern/PatternRendererShould.cs
@@ -0,0 +1,55 @@
+﻿using AdapterPattern.Library;
+using AdapterPattern.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+
+namespace AdapterPattern
+{
+    [TestClass]
+    public class PatternRendererShould
+    {
+        [TestMethod]
+        public void RenderTwoPatterns()
+        {
+            var myRenderer = new PatternRenderer();
+
+            var myList = new List<Pattern>
+                             {
+                                 new Pattern {Id = 1, Name = "Pattern One", Description = "Pattern One Description"},
+                                 new Pattern {Id = 2, Name = "Pattern Two", Description = "Pattern Two Description"}
+                             };
+
+            string result = myRenderer.ListPatterns(myList);
+
+            Console.Write(result);
+
+            int lineCount = result.Count(c => c == '\n');
+            Assert.AreEqual(myList.Count + 2, lineCount);
+        }
+
+        [TestMethod]
+        public void RenderTwoRowsGivenOleDbDataAdapter()
+        {
+            var adapter = new OleDbDataAdapter();
+            adapter.SelectCommand = new OleDbCommand("SELECT * FROM Pattern");
+            adapter.SelectCommand.Connection = 
+                new OleDbConnection(
+                    @"Provider=Microsoft.SQLSERVER.CE.OLEDB.4.0;Data Source=D:\GitHub\DesignPatternsLibrary\AdapterPattern\TestDB.sdf");
+
+            var myRenderer = new DataRenderer(adapter);
+
+            var writer = new StringWriter();
+            myRenderer.Render(writer);
+
+            string result = writer.ToString();
+            Console.Write(result);
+
+            int lineCount = result.Count(c => c == '\n');
+            Assert.AreEqual(4, lineCount);
+        }
+    }
+}
